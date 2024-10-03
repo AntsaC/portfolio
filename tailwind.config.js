81387dbf3b9ccce8e6e07bc50c47bfa584c7e5a4
Antsac <antsafiderana333@gmail.com>
2024-10-03 11:12:08 +0300

    Update : container global horizontal padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,10 +20,13 @@ module.exports = {
       },
       container: {
         center: true,
-        padding: '2rem',
-        screens: {
-          '2xl': 'calc(1320px + 5rem)'
-        }
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
       }
     },
   },
